7cf7cce185cf5ec94c2853b176308843b95f09c6
Surdma <devsurdma@gmail.com>
2026-08-07 04:17:01 +0100

    refactor: provide encapsulated Better Auth service

--- src/modules/auth-v1/auth-v1.module.ts
+++ src/modules/auth-v1/auth-v1.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoggingModule } from '../../common/logging.module.js';
-import { SanitizedLogger } from '../../common/sanitized-logger.js';
-import {
-  APP_CONFIGURATION,
-  AppConfigurationModule,
-  type AppConfiguration,
-} from '../../config/configuration.js';
+import { AppConfigurationModule } from '../../config/configuration.js';
 import { PrismaModule } from '../../infrastructure/prisma/prisma.module.js';
-import { PrismaService } from '../../infrastructure/prisma/prisma.service.js';
-import {
-  AUTH_V1_EMAIL_SENDER,
-  type AuthV1EmailSender,
-} from './auth-v1-email-sender.port.js';
-import {
-  BETTER_AUTH_V1,
-  createAekoBetterAuth,
-} from './better-auth.factory.js';
+import { AUTH_V1_EMAIL_SENDER } from './auth-v1-email-sender.port.js';
+import { BetterAuthV1Service } from './better-auth-v1.service.js';
 import { ResendAuthV1EmailSender } from './resend-auth-v1-email-sender.js';
 
 @Module({
@@ -26,22 +14,8 @@ import { ResendAuthV1EmailSender } from './resend-auth-v1-email-sender.js';
       provide: AUTH_V1_EMAIL_SENDER,
       useExisting: ResendAuthV1EmailSender,
     },
-    {
-      provide: BETTER_AUTH_V1,
-      inject: [
-        APP_CONFIGURATION,
-        PrismaService,
-        AUTH_V1_EMAIL_SENDER,
-        SanitizedLogger,
-      ],
-      useFactory: (
-        configuration: AppConfiguration,
-        prisma: PrismaService,
-        emailSender: AuthV1EmailSender,
-        logger: SanitizedLogger,
-      ) => createAekoBetterAuth(configuration, prisma, emailSender, logger),
-    },
+    BetterAuthV1Service,
   ],
-  exports: [BETTER_AUTH_V1],
+  exports: [BetterAuthV1Service],
 })
 export class AuthV1Module {}
