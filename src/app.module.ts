556151790f5fa783c899b6d742660fdb3700f11c
Surdma <devsurdma@gmail.com>
2026-08-07 03:43:01 +0100

    refactor: use versioned legacy auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { HttpExceptionFilter } from './common/http-exception.filter.js';
 import { LoggingModule } from './common/logging.module.js';
 import { RequestIdMiddleware } from './common/request-id.middleware.js';
 import { AppConfigurationModule } from './config/configuration.js';
-import { AuthModule } from './modules/auth/auth.module.js';
+import { AuthV0Module } from './modules/auth/auth.module.js';
 import { HealthModule } from './modules/health/health.module.js';
 import { InterestsModule } from './modules/interests/interests.module.js';
 import { NotificationsModule } from './modules/notifications/notifications.module.js';
@@ -14,7 +14,7 @@ import { WaitlistModule } from './modules/waitlist/waitlist.module.js';
   imports: [
     AppConfigurationModule.forRoot(),
     LoggingModule,
-    AuthModule,
+    AuthV0Module,
     HealthModule,
     WaitlistModule,
     InterestsModule,
